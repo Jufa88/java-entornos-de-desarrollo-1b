a5c12e118d1afc6e9432c915e7ee072a4936f950
Jufa88 <juanfranmargo8@gmail.com>
2024-03-21 09:53:05 +0100

    first commit

---
+++ modelos/src/modelos/Casa.java
@@ -0,0 +1,93 @@
+package modelos;
+
+import java.util.*;
+
+/**
+ * Representa una casa del mundo real.
+ */
+public class Casa {
+
+    /**
+     * Constructor por defecto
+     */
+    public Casa() {
+    }
+
+    /**
+     * 
+     */
+    public String direccion;
+
+    /**
+     * 
+     */
+    public int numHabitaciones;
+
+    /**
+     * 
+     */
+    public int metrosCuadrados;
+
+    /**
+     * @param direccion 
+     * @param numHabitaciones 
+     * @param metrosCuadrados
+     */
+    public Casa(String direccion, int numHabitaciones, int metrosCuadrados) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public String getDireccion() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @return
+     */
+    public int getNumHabitaciones() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @return
+     */
+    public int getMetrosCuadrados() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @param Direccion
+     */
+    public void setDireccion(String Direccion) {
+        // TODO implement here
+    }
+
+    /**
+     * @param numHabitaciones
+     */
+    public void setNumHabitaciones(int numHabitaciones) {
+        // TODO implement here
+    }
+
+    /**
+     * @param metrosCuadrados
+     */
+    public void setMetrosCuadrados(int metrosCuadrados) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public String toString() {
+        // TODO implement here
+        return "";
+    }
+
+}
---
+++ modelos/src/modelos/Persona.java
@@ -0,0 +1,47 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Persona {
+
+    /**
+     * Default constructor
+     */
+    public Persona() {
+    }
+
+    /**
+     * 
+     */
+    public String dni;
+
+    /**
+     * 
+     */
+    public String nombre;
+
+    /**
+     * 
+     */
+    public String apellido1;
+
+    /**
+     * 
+     */
+    public String apellido2;
+
+    /**
+     * 
+     */
+    public int edad = 0;
+
+    /**
+     * @param dni
+     */
+    public void persona(String dni) {
+        // TODO implement here
+    }
+
+}
